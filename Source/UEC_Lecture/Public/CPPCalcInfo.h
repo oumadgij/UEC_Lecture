4a3a1fbc2fa417e00bc6bf7dc3389de2e201e1ab
oumadgij <106945464+oumadgij@users.noreply.github.com>
2023-05-23 10:53:51 +0900

    構造体

---
+++ Source/UEC_Lecture/Public/CPPCalcInfo.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPPClacType.h"
+#include "CPPCalcInfo.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FCPPCalcInfo
+{
+	GENERATED_BODY()
+
+		ECPPCalcType Type = ECPPCalcType::Add;
+	int32 NumA = 7;
+	int32 NumB = 3;
+};
